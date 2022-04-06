6efda72bcacfeeb34baa7f4513ccf651c8f8abcf
Helder Correia <helder.patrick25@gmail.com>
2022-04-06 21:49:28 +0100

    add Component Dashboard and Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "./components/Dashboard";
 
 export const App = () => (
   <>
     <GlobalStyle />
     <Header />
+    <Dashboard />
   </>
 );
